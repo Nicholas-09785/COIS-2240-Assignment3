3f799974fdbf7fbed0801130b57cae6e54d45028
Nicholas-09785 <npetrelli@live.ca>
2024-11-27 22:38:37 -0500

    Task 2 2. commit

--- Transaction.java
+++ Transaction.java
@@ -1,5 +1,8 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Transaction {
 
@@ -23,6 +26,7 @@ public class Transaction {
             member.borrowBook(book); 
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails); // 2.
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -37,11 +41,25 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails); // 2.
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
     }
-
+    
+    // 2.
+    // saveTransaction saves passed parameter to transaction.txt
+    private void saveTransaction(String transaction) {
+    	try {
+    		BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt", true));
+    		writer.write(transaction + "\n");
+    		writer.flush();
+    		writer.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
     // Get the current date and time in a readable format
     private String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
